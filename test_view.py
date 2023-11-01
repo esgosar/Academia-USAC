6bb4f8fc6fe18e290565dfe9079d06654c4808b2
Estuardo <3354094580901@ingenieria.usac.edu.gt>
2023-10-31 21:11:47 -0600

    alumn_view interface finished

--- test_view.py
+++ test_view.py
@@ -24,5 +24,13 @@ username.pack(side=tk.LEFT)
 image_viewer_canvas = ImageViewerCanvas(master=profile_section, username='user', width=40, height=40)
 image_viewer_canvas.pack(side=tk.RIGHT, padx=10)
 
+# Define the body frame
+body = tk.Frame(root, bg="grey")
+body.pack(expand=True, fill=tk.BOTH)
+
+# Message Label
+message_label = tk.Label(body, text="Sin cursos asignados", bg="grey", fg="white", font=("Helvetica", 24))
+message_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
 # Start the GUI event loop
 root.mainloop()
