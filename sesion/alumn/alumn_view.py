9752cbe20634d82054415d2d2c03e0902b60416c
Estuardo <3354094580901@ingenieria.usac.edu.gt>
2023-10-31 21:51:59 -0600

    sesion/alumn/alumn_view.py

--- sesion/alumn/alumn_view.py
+++ sesion/alumn/alumn_view.py
@@ -1,59 +1,64 @@
 import tkinter as tk
-from tkinter import ttk
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from image_display import ImageViewerCanvas
 
 class AlumnView(tk.Frame):
     def __init__(self, master, switch_view):
         super().__init__(master)
-
-        self.root = tk.Frame(self)
-        self.root.pack(padx=50, pady=50, expand=True)
+        self.root = tk.Frame(self, bg="grey")
+        self.root.pack(expand=True, fill=tk.BOTH)
         self.switch_view = switch_view
 
-        # Barra superior
-        self.barra_superior = tk.Frame(self.root, bg="#87CEEB")
-        self.barra_superior.pack(fill="x")
+        # Header
+        self.header = tk.Frame(self.root, bg="white", height=60)
+        self.header.pack(fill=tk.X, side=tk.TOP)
 
-        self.nombre_usuario = "Ejemplo"
-        self.mensaje_bienvenida = tk.Label(self.barra_superior, text=self.nombre_usuario, font=("Helvetica", 12), bg="#87CEEB", fg="black")
-        self.mensaje_bienvenida.pack(side="right", padx=10, pady=10)
+        self.header_text = tk.Label(self.header, text="Asignar Cursos", bg="white", fg="black", font=("Helvetica", 16))
+        self.header_text.pack(side=tk.LEFT, padx=10)
+        # Bind left-click event to header_text to trigger assign_courses method
+        self.header_text.bind("<Button-1>", self.assign_courses)
+        self.header_text.bind("<Enter>", lambda e: self.header_text.config(cursor="hand2"))
+        self.header_text.bind("<Leave>", lambda e: self.header_text.config(cursor=""))
 
-        self.cursos_button = tk.Button(self.root, text="Mis cursos", font=("Helvetica", 14), bg="blue", fg="white")
-        self.cursos_button.pack(pady=10)
 
-        # Marco para los mosaicos
-        self.mosaicos_frame = tk.Frame(self.root, bg="white")
-        self.mosaicos_frame.pack()
+        self.profile_section = tk.Frame(self.header, bg="white")
+        self.profile_section.pack(side=tk.RIGHT, padx=10, pady=10)
+        # Create a menu with a single option "Cerrar sesión"
+        self.menu = tk.Menu(self.profile_section, tearoff=0)
+        self.menu.add_command(label="Cerrar sesión", command=self.close_session)
 
-        # Ancho y alto uniformes para los mosaicos
-        ancho_mosaico = 200
-        alto_mosaico = 200
+        # Bind right-click event to profile_section to display the menu
+        self.profile_section.bind("<Button-1>", self.show_menu)
+        self.profile_section.bind("<Enter>", lambda e: self.profile_section.config(cursor="hand2"))
+        self.profile_section.bind("<Leave>", lambda e: self.profile_section.config(cursor=""))
 
-        # Estilos para los cursos
-        curso_style = {"bg": "white", "fg": "black", "font": ("Helvetica", 14, "bold")}
-        cursos = [
-            {"nombre": "Matemáticas", "imagen": "math.png"},
-            {"nombre": "Historia", "imagen": "history.png"},
-            {"nombre": "Programación", "imagen": "python.png"},
-            {"nombre": "Física Básica", "imagen": "art.png"},
-            {"nombre": "Curso 5", "imagen": "image5.png"}  # Agrega más cursos si es necesario
-        ]
+        self.username = tk.Label(self.profile_section, text="Username", bg="white", fg="black", font=("Helvetica", 16))
+        self.username.pack(side=tk.LEFT)
+        # Bind the same events to username label
+        self.username.bind("<Button-1>", self.show_menu)
+        
+        self.image_viewer_canvas = ImageViewerCanvas(master=self.profile_section, username='user', width=40, height=40)
+        self.image_viewer_canvas.pack(side=tk.RIGHT, padx=10)
+        # Bind the same events to image_viewer_canvas
+        self.image_viewer_canvas.bind("<Button-1>", self.show_menu)
 
-        for i, curso_data in enumerate(cursos):
-            mosaico = ttk.Frame(self.mosaicos_frame, padding=10)
-            mosaico.grid(row=i // 3, column=i % 3, padx=20, pady=10)
+        # Border line
+        self.border_line = tk.Frame(self.root, height=1, bg='black')
+        self.border_line.pack(fill=tk.X, side=tk.TOP)
 
-            # Crear etiqueta para el nombre del curso
-            nombre_label = tk.Label(mosaico, text=curso_data["nombre"], **curso_style)
-            nombre_label.pack()
-
-            # Crear botón para inscribirse en el curso
-            inscribirse_button = tk.Button(mosaico, text="Inscribirse", command=lambda curso=curso_data["nombre"]: self.on_mosaico_click(curso))
-            inscribirse_button.pack()
-
-         # Button at top-left
-        self.top_left_button = tk.Button(self.barra_superior, text="Cerrar sesión", command=self.close_session)
-        self.top_left_button.pack(side='left')
-
+        # Body
+        self.message_label = tk.Label(self.root, text="Sin cursos asignados", bg="grey", fg="white", font=("Helvetica", 24))
+        self.message_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        
+    def show_menu(self, event):
+        # Display menu at the position of the left-click
+        try:
+            self.menu.post(event.x_root, event.y_root)
+        finally:
+            # make sure to release the grab (Tk 8.0a1 only)
+            self.menu.grab_release()
 
     def close_session(self):
         import sys
@@ -61,7 +66,7 @@ class AlumnView(tk.Frame):
         sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
         from main_view import MainView  # Conditional import
         self.switch_view('MainView')
-    
-  
-    def on_mosaico_click(self, curso):
-        print(f"Has hecho clic en el curso: {curso}")
+
+    def assign_courses(self, event):
+        # Logic to handle "Asignar Cursos" click
+        print("Asignar Cursos clicked")
