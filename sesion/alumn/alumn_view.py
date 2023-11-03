c9b75e0203aea4139ceb0cbca1b9357a361b4665
Estuardo <3354094580901@ingenieria.usac.edu.gt>
2023-11-02 19:17:47 -0600

    Cerrar session interface updated + AssignView Interface implemented

--- sesion/alumn/alumn_view.py
+++ sesion/alumn/alumn_view.py
@@ -4,6 +4,35 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from image_display import ImageViewerCanvas
 
+class CerrarSesionModal(tk.Toplevel):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.overrideredirect(True)  # Removes the title bar
+        
+        # Adjust the dimensions as per your requirement
+        width, height = 150, 20  # for example, 150x50
+        screen_width, screen_height = self.winfo_screenwidth(), self.winfo_screenheight()
+        x_offset, y_offset = screen_width - width + 40, height + 42
+        self.geometry(f"{width}x{height}+{x_offset}+{y_offset}")
+        
+        # Create a label for the "Cerrar sesión" option with red text and a white background
+        self.cerrar_sesion_label = tk.Label(self, text="Cerrar sesión", fg='red')
+        self.cerrar_sesion_label.pack()
+        
+        # Bind the left mouse button click event to the on_cerrar_sesion method
+        self.cerrar_sesion_label.bind("<Button-1>", lambda e: self.on_cerrar_sesion())
+        
+        # Change the cursor to a hand cursor when it hovers over the label
+        self.cerrar_sesion_label.bind("<Enter>", lambda e: self.cerrar_sesion_label.config(cursor="hand2"))
+        self.cerrar_sesion_label.bind("<Leave>", lambda e: self.cerrar_sesion_label.config(cursor=""))
+
+        self.protocol("WM_DELETE_WINDOW", self.destroy)  # Handle the modal closure
+
+    def on_cerrar_sesion(self):
+        self.destroy()
+        self.master.close_session()
+        self.destroy()
+
 class AlumnView(tk.Frame):
     def __init__(self, master, switch_view):
         super().__init__(master)
@@ -17,14 +46,15 @@ class AlumnView(tk.Frame):
 
         self.header_text = tk.Label(self.header, text="Asignar Cursos", bg="white", fg="black", font=("Helvetica", 16))
         self.header_text.pack(side=tk.LEFT, padx=10)
+
         # Bind left-click event to header_text to trigger assign_courses method
         self.header_text.bind("<Button-1>", self.assign_courses)
         self.header_text.bind("<Enter>", lambda e: self.header_text.config(cursor="hand2"))
         self.header_text.bind("<Leave>", lambda e: self.header_text.config(cursor=""))
 
-
         self.profile_section = tk.Frame(self.header, bg="white")
         self.profile_section.pack(side=tk.RIGHT, padx=10, pady=10)
+
         # Create a menu with a single option "Cerrar sesión"
         self.menu = tk.Menu(self.profile_section, tearoff=0)
         self.menu.add_command(label="Cerrar sesión", command=self.close_session)
@@ -53,12 +83,9 @@ class AlumnView(tk.Frame):
         self.message_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
         
     def show_menu(self, event):
-        # Display menu at the position of the left-click
-        try:
-            self.menu.post(event.x_root, event.y_root)
-        finally:
-            # make sure to release the grab (Tk 8.0a1 only)
-            self.menu.grab_release()
+        self.modal = CerrarSesionModal(master=self)
+        # Optionally, set focus to the modal to ensure it stays on top
+        self.modal.focus_set()
 
     def close_session(self):
         import sys
@@ -67,6 +94,6 @@ class AlumnView(tk.Frame):
         from main_view import MainView  # Conditional import
         self.switch_view('MainView')
 
-    def assign_courses(self, event):
-        from assign_view import AssignView  # Conditional import
+    def assign_courses(self, event=None):
+        from sesion.alumn.assign_view import AssignView  # Conditional import
         self.switch_view('AssignView')
--- sesion/alumn/assign_view.py
+++ sesion/alumn/assign_view.py
@@ -1,4 +1,47 @@
 import tkinter as tk
+import json
+
+class CourseFrame(tk.Frame):
+    def __init__(self, master, course_code, course_data):
+        super().__init__(master)
+        self.config(borderwidth=1, relief="solid")
+
+        # Display course code and name in a larger font
+        name_label = tk.Label(self, text=f"{course_code} - {course_data['Nombre']}", font=("Helvetica", 18))
+        name_label.grid(row=0, column=0, columnspan=2, sticky='w', padx=5, pady=5)
+
+        # Display other course data
+        for i, (key, value) in enumerate(course_data.items(), start=1):
+            if key != 'Nombre':  # skip the 'Nombre' key since it's already displayed
+                label = tk.Label(self, text=f"{value}")
+                label.grid(row=i, column=0, sticky='w', padx=5, pady=5)
+
+        # Create Delete button
+        assign_button = tk.Button(self, text="Asignar", command=self.assign_course)
+        assign_button.grid(row=i+1, column=0, columnspan=2, padx=5, pady=5)
+
+    def assign_course(self):
+        print("Hello")
 
 class AssignView(tk.Frame):
-    pass
+    def __init__(self, master, switch_view):
+        super().__init__(master)
+        self.switch_view = switch_view
+        self.root = tk.Frame(self, bg="grey")
+        self.root.pack(expand=True, fill=tk.BOTH)
+        self.display_courses()
+
+    def display_courses(self):
+        try:
+            with open('courses.json', 'r') as f:
+                courses_dict = json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError):
+            courses_dict = {}
+
+        self.body_frame = tk.Frame(self.root)
+        self.body_frame.pack(fill=tk.BOTH, expand=True)
+
+        for i, (course_code, course_data) in enumerate(courses_dict.items()):
+            course_frame = CourseFrame(self.body_frame, course_code, course_data)
+            row, col = divmod(i, 3)  # Arrange courses in a grid with 3 columns
+            course_frame.grid(row=row, column=col, padx=10, pady=10)
