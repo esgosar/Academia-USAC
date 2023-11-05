79b1fc22bbe07e2f2f65d2efbb4b51f0de549c2c
Estuardo <3354094580901@ingenieria.usac.edu.gt>
2023-11-04 18:14:16 -0600

    Old mail.py, now wrapped into a class and added all mail notifications

---
+++ notifications.py
@@ -0,0 +1,119 @@
+import os
+import yagmail
+
+class Mail:
+    def __init__(self):
+        self.yag = yagmail.SMTP(
+            user='3354094580901@ingenieria.usac.edu.gt', 
+            password=os.getenv("PASSWORD")
+        )
+
+    def block(mail, name):
+        contents = f'''
+            <html>
+                <body>
+                    <p>
+                        {name},<br><br>
+                        Por seguridad, hemos bloqueado su cuenta debido a múltiples intentos fallidos de inicio de sesión.
+                        Para desbloquear su cuenta, por favor contacte con el administrador.<br><br>
+                        Atentamente,<br>
+                        El equipo de Soporte
+                    </p>
+                </body>
+            </html>
+            '''
+
+        yag.send(
+            to=mail,
+            subject='Cuenta Bloqueada',
+            contents=contents,
+            headers={'From': 'Academia USAC <3354094580901@ingenieria.usac.edu.gt>'}
+        )
+
+    def confirm(mail, name):
+        contents = f'''
+            <html>
+                <body>
+                    <p>
+                        {name},<br><br>
+                        ¡Gracias por registrarse!<br><br>
+                        Atentamente,<br>
+                        El equipo de Soporte
+                    </p>
+                </body>
+            </html>
+            '''
+
+        yag.send(
+            to=mail,
+            subject='Cuenta Registrada',
+            contents=contents,
+            headers={'From': 'Academia USAC <3354094580901@ingenieria.usac.edu.gt>'}
+        )
+
+    def recovery(mail, name, password):
+        contents = f'''
+            <html>
+                <body>
+                    <p>
+                        {name},<br><br>
+                        ¡Hemos recibido una solicitud para restablecer su contraseña.<br>
+                        Su contraseña es: {password}
+                        Si no ha solicitado un restablecimiento de contraseña, ignore este correo electrónico.<br><br>
+                        Atentamente,<br>
+                        El equipo de Soporte
+                    </p>
+                </body>
+            </html>
+            '''
+
+        yag.send(
+            to=mail,
+            subject='Recuperación de Contraseña',
+            contents=contents,
+            headers={'From': 'Academia USAC <3354094580901@ingenieria.usac.edu.gt>'}
+        )
+
+    def assignation(mail, name, course):
+        contents = f'''
+            <html>
+                <body>
+                    <p>
+                        {name},<br><br>
+                        Nos complace informarle que ha sido asignado al curso {course}.<br><br>
+                        ¡Esperamos que disfrute su aprendizaje!<br><br>
+                        Atentamente,<br>
+                        El equipo de Coordinación Académica
+                    </p>
+                </body>
+            </html>
+            '''
+
+        yag.send(
+            to=mail,
+            subject='Curso Asignado',
+            contents=contents,
+            headers={'From': 'Academia USAC <3354094580901@ingenieria.usac.edu.gt>'}
+        )
+
+    def unassignation(mail, name, course):
+        contents = f'''
+            <html>
+                <body>
+                    <p>
+                        {name},<br><br>
+                        Le confirmamos que ha eliminado el curso {course} de su lista de cursos asignados.<br><br>
+                        Si tiene alguna pregunta o necesita asistencia, no dude en contactarnos.<br><br>
+                        Atentamente,<br>
+                        El equipo de Coordinación Académica
+                    </p>
+                </body>
+            </html>
+            '''
+
+        yag.send(
+            to=mail,
+            subject='Curso Desasignado',
+            contents=contents,
+            headers={'From': 'Academia USAC <3354094580901@ingenieria.usac.edu.gt>'}
+        )
